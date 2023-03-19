4e43d2ea5747e921be988fff6d3fd342f1396cb1
Zynh0722 <Zynh0722@gmail.com>
2023-03-19 04:07:07 -0700

    rouille testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use rouille::Request;
+use rouille::Response;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    rouille::start_server("0.0.0.0:5369", move |req| {
+        let mut data = req.data().expect("shouldve had data here...");
+
+        let mut buf = Vec::new();
+        match data.read_to_end(&mut buf) {
+            Ok(_) => (),
+            Err(_) => return Response::text("Failed to read body")
+        };
+
+        if let Ok(str) = String::from_utf8(buf) {
+            println!("{}\n{}", req.url(), str);
+        }
+
+        Response::text("")
+    })
 }
+
