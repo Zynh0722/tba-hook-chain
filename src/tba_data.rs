63a20004adc849c9b594c95f5395b026542f5d59
Zynh0722 <Zynh0722@gmail.com>
2023-03-20 20:54:16 -0700

    more data serdeified, 4 left

--- src/tba_data.rs
+++ src/tba_data.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 
 type TeamNumber = u16;
+type Year = u16;
 
 #[derive(Deserialize, Debug)]
 #[serde(tag = "message_type", content = "message_data")]
@@ -10,28 +11,10 @@ pub enum TBAData {
     Ping(Ping),
     Broadcast(Broadcast),
     UpcomingMatch(UpcomingMatch),
+    ScheduleUpdated(ScheduleUpdated),
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Verification {
-    pub verification_key: String,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Ping {
-    pub title: String,
-    #[serde(rename = "desc")]
-    pub description: String,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Broadcast {
-    pub title: String,
-    #[serde(rename = "desc")]
-    pub description: String,
-    pub url: String,
-}
-
+// https://www.thebluealliance.com/apidocs/webhooks#upcoming_match
 #[derive(Deserialize, Debug)]
 pub struct UpcomingMatch {
     pub event_key: String,
@@ -51,4 +34,71 @@ pub struct Webcast {
     pub channel: String,
     pub date: Option<String>,
     pub file: Option<String>,
+}
+
+// https://www.thebluealliance.com/apidocs/webhooks#match_score
+// TODO
+
+// https://www.thebluealliance.com/apidocs/webhooks#match_video
+// TODO
+
+// https://www.thebluealliance.com/apidocs/webhooks#starting_comp_level
+// TODO
+
+// https://www.thebluealliance.com/apidocs/webhooks#alliance_selection
+// TODO
+
+// https://www.thebluealliance.com/apidocs/webhooks#awards_posted
+#[derive(Deserialize, Debug)]
+pub struct AwardsPosted {
+    pub event_key: String,
+    pub team_key: Option<String>,
+    pub event_name: String,
+    pub awards: Vec<Award>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Award {
+    pub name: String,
+    pub award_type: u8,
+    pub event_key: String,
+    pub recipient_list: Vec<Recipient>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Recipient {
+    pub team_key: Option<String>,
+    pub awardee: Option<String>,
+    pub year: Year,
+}
+
+// https://www.thebluealliance.com/apidocs/webhooks#schedule_updated
+#[derive(Deserialize, Debug)]
+pub struct ScheduleUpdated {
+    pub event_key: String,
+    pub event_name: String,
+    pub first_match_time: Option<u32>,
+}
+
+// https://www.thebluealliance.com/apidocs/webhooks#ping
+#[derive(Deserialize, Debug)]
+pub struct Ping {
+    pub title: String,
+    #[serde(rename = "desc")]
+    pub description: String,
+}
+
+// https://www.thebluealliance.com/apidocs/webhooks#broadcast
+#[derive(Deserialize, Debug)]
+pub struct Broadcast {
+    pub title: String,
+    #[serde(rename = "desc")]
+    pub description: String,
+    pub url: String,
+}
+
+// https://www.thebluealliance.com/apidocs/webhooks#verification
+#[derive(Deserialize, Debug)]
+pub struct Verification {
+    pub verification_key: String,
 }
